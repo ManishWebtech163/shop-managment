86f62c13be706ba307e0b1b5c618a0c6e1224774
ManishWebtech163 <panchalmanish163@gmail.com>
2024-04-13 17:25:06 +0530

    update proxy link

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://shop-managment-backend.vercel.app/',
         changeOrigin: true,
       },
     }
